3fdd3dbb5f5fe2a040b9104f9a57e26675e37b16
Pitágoras Alves <alves.pitagoras@gmail.com>
2017-09-11 03:23:09 -0300

    6th plot: correlation between projects as coordinator and total projects

--- 6-correlation.py
+++ 6-correlation.py
@@ -18,5 +18,9 @@ np.random.seed(int(datetime.now().day + datetime.now().second))
 sns.set(color_codes=True)
 
 df = pd.read_csv("pesquisadores.csv", sep=";")
+df['totalProjetos'] = df.apply(lambda row: row['internos']+row['externos'],
+  axis=1)
 plot = sns.lmplot(x="internos", y="externos", data=df, 
-                  x_jitter=0.2, y_jitter=0.21, robust=True);
+                  x_jitter=0.2, y_jitter=0.21, robust=False);
+plot2 = sns.lmplot(x="totalProjetos", y="coordenador", data=df, 
+                  x_jitter=0.2, y_jitter=0.21, order=3);
