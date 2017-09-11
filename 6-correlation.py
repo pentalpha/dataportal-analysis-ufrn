c85734449325ad2c9fc6f2a4f55555aa7bc53138
Pitágoras Alves <alves.pitagoras@gmail.com>
2017-09-11 01:51:12 -0300

    6th plot: correlation between internal and external projects

---
+++ 6-correlation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 01:10:31 2017
+
+@author: pitagoras
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+np.random.seed(int(datetime.now().day + datetime.now().second))
+
+sns.set(color_codes=True)
+
+df = pd.read_csv("pesquisadores.csv", sep=";")
+plot = sns.lmplot(x="internos", y="externos", data=df, 
+                  x_jitter=0.2, y_jitter=0.21, robust=True);
